6a4b28e9b621b2f980ea22e35d5df3c9ec887157
Jessicalima1 <jee_014@hotmail.com>
2021-04-08 19:37:01 -0300

    Dados de usuário

--- src/main/java/com/cadastrousuario/crm/controller/UsuarioController.java
+++ src/main/java/com/cadastrousuario/crm/controller/UsuarioController.java
@@ -1,8 +1,12 @@
 package com.cadastrousuario.crm.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cadastrousuario.crm.model.Usuario;
 
 @RestController
 @RequestMapping("/usuários")
--- src/main/java/com/cadastrousuario/crm/model/Usuario.java
+++ src/main/java/com/cadastrousuario/crm/model/Usuario.java
@@ -1,5 +1,96 @@
 package com.cadastrousuario.crm.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
 public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+
+	@Column(nullable = false)
+	private String Nome;
+
+	@Column(nullable = false)
+	private String CPF;
+
+	@Column(nullable = false)
+	private String Nascimento;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Id == null) ? 0 : Id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (Id == null) {
+			if (other.Id != null)
+				return false;
+		} else if (!Id.equals(other.Id))
+			return false;
+		return true;
+	}
+
+	@Column(nullable = false)
+	private String Email;
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getNome() {
+		return Nome;
+	}
+
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+
+	public String getCPF() {
+		return CPF;
+	}
+
+	public void setCPF(String cPF) {
+		CPF = cPF;
+	}
+
+	public String getNascimento() {
+		return Nascimento;
+	}
+
+	public void setNascimento(String nascimento) {
+		Nascimento = nascimento;
+	}
+
+	public String getEmail() {
+		return Email;
+	}
+
+	public void setEmail(String email) {
+		Email = email;
+	}
 
 }
