3fc32e8cd17b821ddda0e8e53312d4c9f364b2ac
Jessicalima1 <jee_014@hotmail.com>
2021-04-08 20:57:33 -0300

    cadastrando usuários

--- src/main/java/com/cadastrousuario/crm/controller/UsuarioController.java
+++ src/main/java/com/cadastrousuario/crm/controller/UsuarioController.java
@@ -3,8 +3,12 @@ package com.cadastrousuario.crm.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cadastrousuario.crm.model.Usuario;
@@ -13,12 +17,19 @@ import com.cadastrousuario.crm.repository.UsuarioRepository;
 @RestController
 @RequestMapping("/usuários")
 public class UsuarioController {
-	
+
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 
 	@GetMapping
 	public List<Usuario> listar() {
 		return usuarioRepository.findAll();
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Usuario adicionar(@RequestBody Usuario usuario){
+		return usuarioRepository.save(usuario);
+
 	}
 }
